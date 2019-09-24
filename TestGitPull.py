23c4e17b2900a6377bbc088aa40f3be4d237cfe6
csant7 <snowskier1998@gmail.com>
2019-09-24 19:26:25 -0400

    Added Some Mock

--- TestGitPull.py
+++ TestGitPull.py
@@ -5,8 +5,11 @@ Created on Sep 18, 2019
 '''
 
 import unittest
+from unittest import mock
+from unittest.mock import MagicMock as Mock
 from GitPull import gitHubAPI
-
+from unittest.mock import patch
+m = Mock()
 
 class Object(object):
     def __init__(self, content):
@@ -20,9 +23,11 @@ class testGitPull(unittest.TestCase):
         m.json.return_value = jsonObj
         gitResults = []
 
+        daResults = [Mock(), Mock(), Mock()]
         
         gitResults.append(json.loads('[{"name" : "SSW-567-HelloWorld"}. {"name": "SSW-567-HW2a"}, {"name": "SSW-567-HW4a"}, {"name": "Triangle-Classification"}]'))
         gitResults.append(json.loads('[ { "commit" : "1" }, { "commit" : "9" }, { "commit" : "2" }, { "commit" : "1" } ]'))
+        m.json.side_effect = daResults
 
 if __name__ == '__main__':
     unittest.main(exit = False)
