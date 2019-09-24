7a876eeb750697051a648f34376e938203fbe991
csant7 <snowskier1998@gmail.com>
2019-09-24 19:15:53 -0400

    Removed Mock(meant to created new branch)

--- TestGitPull.py
+++ TestGitPull.py
@@ -5,11 +5,7 @@ Created on Sep 18, 2019
 '''
 
 import unittest
-from unittest import mock
-from unittest.mock import MagicMock as Mock
 from GitPull import gitHubAPI
-from unittest.mock import patch
-m = Mock()
 
 
 class Object(object):
@@ -23,12 +19,10 @@ class testGitPull(unittest.TestCase):
         jsonObj = json.loads(json)
         m.json.return_value = jsonObj
         gitResults = []
-        
-        daResults = [Mock(), Mock(), Mock()]
+
         
         gitResults.append(json.loads('[{"name" : "SSW-567-HelloWorld"}. {"name": "SSW-567-HW2a"}, {"name": "SSW-567-HW4a"}, {"name": "Triangle-Classification"}]'))
         gitResults.append(json.loads('[ { "commit" : "1" }, { "commit" : "9" }, { "commit" : "2" }, { "commit" : "1" } ]'))
-        m.json.side_effect = daResults
 
 if __name__ == '__main__':
     unittest.main(exit = False)
